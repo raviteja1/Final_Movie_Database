2698d386590b1e1c4e342c613efd52747b647647
raviteja <pawanatd2014@gmail.com>
2020-07-04 17:00:47 +0530

    changed bg o body of html and home button fix

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
     
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: '#070b14'}}>
-  <a className="navbar-brand text-warning" href="#" >Home</a>
+  <a className="navbar-brand text-warning" href="Home" >Home</a>
   <a href="https://ravitejathoudishetty.herokuapp.com" target="_blank" className="text-warning font-weight-bold text-warning">
     Covid Live Data</a>
 </nav>
