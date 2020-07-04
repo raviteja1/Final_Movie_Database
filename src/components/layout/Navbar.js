1794ef79b53c7715aa49da3e2bf4489451886a43
raviteja <pawanatd2014@gmail.com>
2020-07-04 22:24:09 +0530

    added soldier jppg in layout changed navbar by adding div col md 10 and app.css .soldier class

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import Soldier from './Soldier.jpg';
 
 function Navbar() {
   return (
@@ -8,7 +9,11 @@ function Navbar() {
   <a className="navbar-brand text-warning" href="Home" >Home</a>
   <a href="https://ravitejathoudishetty.herokuapp.com" target="_blank" className="text-warning font-weight-bold text-warning">
     Covid Live Data</a>
+    <div class="col-md-10 text-right">
+    <img src={Soldier} className="Soldier" alt="Poster"/></div>
+    {/* //<<img src="Soldier"></img> */}
 </nav>
+
     </div>
 
   )
